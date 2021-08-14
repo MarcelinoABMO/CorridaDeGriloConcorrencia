2212570933b37163d447345fa40ec194e87acbe8
MarcelinoABMO <marcelino.jesus@gmail.com>
2021-08-14 16:10:44 -0300

    Update Grilo.java
    Implementar a função Chegar(); implementar o semáforo

--- src/corridadegriloconcorrencia/Grilo.java
+++ src/corridadegriloconcorrencia/Grilo.java
@@ -21,15 +21,32 @@ public class Grilo extends Thread{
     private int pulos;          //armazena a quantidade de pulos dados
     public int getPulos() { return this.pulos; }
 
+    private Semaphore semaphore;//sincroniza as threads evitando concorrencia.
+    
+    //registra a ordem de chegada no ranking
+    private int posicaoChegada;
+    public int getPosicaoChegada() { return this.posicaoChegada; }
+    
+    //id da equipe do grilo
+    private int idEquipe;
+    public int getEquipe() { return this.idEquipe; }
+    
+    //controla a ordem de chegada dos grilos. precisa ser sincronizado.
+    public volatile static int contadorDeChegada = 0;
+    
     /**
      * Construtor da classe.
      * @param index posição da instancia no array e identificação do objeto.
      * @param distanciaMax posição da linha de chegada.
+     * @param idEquipe identificação da equipe do grilo.
+     * @param semaphore passa a referência de um semaforo para todas as instancias de grilo.
      */
-    public Grilo(int index, int distanciaMax){
+    public Grilo(int index, int distanciaMax, int idEquipe, Semaphore semaphore){
         this.indice = index;
 	this.distanciaMax = distanciaMax;
         this.random = new Random();
+        this.idEquipe = idEquipe;
+        this.semaphore = semaphore;
     }
     
     /**
@@ -40,16 +57,16 @@ public class Grilo extends Thread{
         //loop que mantem a thread executando até que o grilo alcance a linha de chegada
         while(posicao < distanciaMax)
         {
-            Pular();
-
             try
             {
                 //interrompe a execução da thread e libera o processador pelo
                 //período de tempo determinado pelo parâmetro do método
                 Thread.sleep(speed);
             } catch (InterruptedException ex){
-                System.out.println("deu merda no sleep da thread");
+                Logger.getLogger(Grilo.class.getName()).log(Level.SEVERE, null, ex);
             }
+            
+            Pular();
         }
     }
     
@@ -67,8 +84,38 @@ public class Grilo extends Thread{
         //incrementa a quantidade de pulos em 1 a cada iteração
 	pulos++;
         
+        //registra a chegada
+        if (posicao >= distanciaMax)
+            Chegar();
+        else
         //imprime um log com as informações da iteração atual
-        System.out.println("O Grilo_" + indice + " pulou " + pegaPulo +
-                "cm e ja percorreu " + posicao + "cm");
+        System.out.println("O Grilo_" + indice + " do time " + (idEquipe + 1) + 
+                " pulou " + pegaPulo + "cm e ja percorreu " + posicao + "cm");
+    }
+    
+    /**
+     * Registra a chegada do grilo ao cruzar a linha e sua posição no ranking.
+     */
+    void Chegar(){
+        //Registra a posição de chegada do grilo,
+        //evitando concorrência de acesso com outras threads
+        //Utilizamos o exemplo do jantar dos filósofos nesta construção, porém não
+        //liberamos a thread com um sleep pois o intuito é garantir a ordem de chegada
+        //registrando-a o mais rápido possível, ou seja, assim que o processador estiver disponível.
+        //Pode não ser a melhor alternativa pois em caso de deadlock, este while true
+        //se encarregará de piorar as coisas gerando um loop infinito.
+        while(true) {
+            if (semaphore.tryAcquire())
+            {
+                contadorDeChegada++;
+                this.posicaoChegada = contadorDeChegada;
+                
+                System.out.println("-> O Grilo_" + indice + " do time " + (idEquipe + 1) +
+                        " foi o " + posicaoChegada +"º colocado com " + pulos + " pulos.");
+                
+                semaphore.release();
+                break;
+            }
+        }
     }
 }
